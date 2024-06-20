b652380893d9558d93a108056126d072cb23f184
Thomas fadi <thomasfadi2000@gmail.com>
2024-06-20 14:08:31 +0200

    new_commet

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:etbaany/Screenas/Nav_bar.dart/Navigation_bar.dart';
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -10,5 +10,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    )}}
+    return MaterialApp(home:NavBar()
+    ); 
+  }
+}
